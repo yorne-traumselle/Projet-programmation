ea7597043c899bb50b2345fcb9718ed0c85bdc1c
Your Name <mapyoune@gmail.com>
2025-05-26 23:23:12 +0200

    update de Cell et Map, j'ajoute les commentaires demain inshaAllah

--- src/game/Cell.h
+++ src/game/Cell.h
@@ -1,28 +1,39 @@
 #pragma once
 
 #include "Map.h"
+//#include "../entity/Spell.h"
 
 
 class Cell : public Map{
 
   private :
 
-    const float m_cell_size; //taille d'une tuile
-
     bool empty; // true si la case est vide
                 // false sinon
     
-    int tile; //texture de la tile en int
-    Vector2<float> m_position;          //Position de la tuile dans le monde
+    unsigned int m_tile; //texture de la tile en int
+
+    Vector2<unsigned int> m_position;//position dans la matrice de cells
+
+    Entity * m_entity;//adresse de l entite qui marche sur la cell
   
   public :
+
+    Cell(unsigned int row , unsigned int column, unsigned int tile);
     
-    const float getCellSize() const;
+    const float getSize() const;
 
     const bool occupied() const;
 
     const int getTile() const;
 
     void setTile(const int & tileValue);
+
+    //doit etre appelee quand une entity entre ou quitte une cellule
+    //Return nullptr si personne n'est sur la cell
+    void steppedOnBy(Entity * entity);
+
+    Entity * Cell::whoIsSteppingOnMe();
     
+    // void doSpell(Spell * spell);
 };
