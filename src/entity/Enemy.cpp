fc3661b6d49baa3ee981ca98c7a2492941d1e184
yorne-traumselle <aquaalphaazer@gmail.com>
2025-05-27 14:19:37 +0200

    getType added

--- src/entity/Enemy.cpp
+++ src/entity/Enemy.cpp
@@ -3,7 +3,25 @@
 //
 #include "Enemy.h"
 #include "Spells.h"
-Enemy::Enemy(Map * map, Vector2<unsigned int>& position, const Vector2<float>& size, const std::string &filename, const std::string &nameEntity, const int HP, const int MP, const int attack, const int defense): Fighter(map, position, size, filename, nameEntity, HP, MP, attack, defense) {
-    m_fighterStatus = notInFight; // Enemies are not in fight by default
+#include "Hero.h"
 
+Enemy::Enemy(Map * map, Vector2<unsigned int>& position, const Vector2<float>& size, const std::string &filename, const std::string &nameEntity, const int HP, const int MP, const int attack, const int defense, Hero* hero): Fighter(map, position, size, filename, nameEntity, HP, MP, attack, defense), m_hero(hero) {
+    m_fighterStatus = notInFight; // Enemies are not in fight by default
 }
+
+
+
+void Enemy::startTurn() {
+    Fighter::startTurn();
+    if (m_hero->getTile().calcDistance(m_tile) <= m_spells[0]->getMaxRange() ) {
+        useSpell(m_hero->getTile(), 0);
+        return;
+    }
+    else {
+        Vector2<unsigned int> where = m_tile.findClosestReachableFreeCell(m_map->getMatriceBool(),
+            m_hero->getTile(),
+            m_MP[1]);
+        setPosition(where)
+    }
+}
+
